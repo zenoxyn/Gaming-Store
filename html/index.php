582fa47ea9d7a55bac72fc7d190efd41576b493b
zenoxyn <zenoxyn.211@gmail.com>
2025-11-18 11:37:08 +0700

    update setup tailwind

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../css/output.css" rel="stylesheet">
+</head>
+<body>
+  <?php
+  include "header.html";
+  ?>
+  <h1 class="text-3xl font-bold underline text-green-500">
+    Hello world!
+  </h1>
+  <p class="text-amber-950">apappa</p>
+  <p class="text-amber-300 text-center">asu</p>
+
+  <div class="container border-green-500 ">oke</div>
+
+  <?php
+  include "footer.html";
+  ?>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./html/**/*.{html,php}", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
