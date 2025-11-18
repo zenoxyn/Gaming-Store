1038a97cc2025e9565c4ebde88d260f01ef028e2
zenoxyn <zenoxyn.211@gmail.com>
2025-11-18 17:55:24 +0700

    header v1.0 done

--- html/index.php
+++ html/index.php
@@ -4,21 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../css/output.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css">
 </head>
-<body>
-  <?php
-  include "header.html";
-  ?>
-  <h1 class="text-3xl font-bold underline text-green-500">
-    Hello world!
-  </h1>
-  <p class="text-amber-950">apappa</p>
-  <p class="text-amber-300 text-center">asu</p>
+<body class="text-white">
+  <?php include "header.html";?>
+  
 
-  <div class="container border-green-500 ">oke</div>
-
-  <?php
-  include "footer.html";
-  ?>
+  <?php include "footer.html";?>
 </body>
 </html>
